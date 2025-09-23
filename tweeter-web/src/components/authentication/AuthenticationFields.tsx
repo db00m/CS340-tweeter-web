886114f499784884e6d146b26cc54c64a863fcb2
david.bloomfield <david.s.bloomfield@gmail.com>
2025-09-23 15:36:09 -0600

    create AuthenticationFields component

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from "react";
+
+interface Props {
+    onEnter: () => void;
+    onPasswordChange: (value: string) => void;
+    onAliasChange: (value: string) => void;
+    isBottom?: string;
+}
+
+const AuthenticationFields = ({ onEnter, onPasswordChange, onAliasChange, isBottom }: Props) => {
+
+    const handleKeyDown = useCallback((event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            onEnter();
+        }
+    }, [onEnter]);
+
+    return (
+        <>
+            <div className="form-floating">
+                <input
+                    type="text"
+                    className="form-control"
+                    size={50}
+                    id="aliasInput"
+                    placeholder="name@example.com"
+                    onKeyDown={handleKeyDown}
+                    onChange={(event) => onAliasChange(event.target.value)}
+                />
+                <label htmlFor="aliasInput">Alias</label>
+            </div>
+            <div className="form-floating">
+                <input
+                    type="password"
+                    className={`form-control ${isBottom ? 'bottom' : ''}`}
+                    id="passwordInput"
+                    placeholder="Password"
+                    onKeyDown={handleKeyDown}
+                    onChange={(event) => onPasswordChange(event.target.value)}
+                />
+                <label htmlFor="passwordInput">Password</label>
+            </div>
+        </>
+    );
+}
+
+export default AuthenticationFields;
