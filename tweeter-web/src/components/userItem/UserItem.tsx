bd19719dc8441432fe65a1d607f56f2bf209f490
david.bloomfield <david.s.bloomfield@gmail.com>
2025-09-24 17:16:49 -0600

    use UserNavigationHooks throughout application

--- tweeter-web/src/components/userItem/UserItem.tsx
+++ tweeter-web/src/components/userItem/UserItem.tsx
@@ -1,53 +1,14 @@
-import { Link, useNavigate } from "react-router-dom";
-import { AuthToken, User, FakeData } from "tweeter-shared";
-import { useMessageActions } from "../toaster/MessageHooks";
-import {useUserInfo, useUserInfoActions} from "../userInfo/UserInfoHooks";
+import { Link } from "react-router-dom";
+import { User  } from "tweeter-shared";
+import {useUserNavigation} from "../userNavigation/UserNavigationHooks";
 
 interface Props {
   user: User;
   featurePath: string;
 }
 
-const UserItem = (props: Props) => {
-  const { displayErrorMessage } = useMessageActions();
-  const { displayedUser, authToken } = useUserInfo();
-  const { setDisplayedUser } = useUserInfoActions();
-
-  const navigate = useNavigate();
-
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const toUser = await getUser(authToken!, alias);
-
-      if (toUser) {
-        if (!toUser.equals(displayedUser!)) {
-          setDisplayedUser(toUser);
-          navigate(`${props.featurePath}/${toUser.alias}`);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to get user because of exception: ${error}`
-      );
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
+const UserItem = ({ user, featurePath }: Props) => {
+  const navigateToUser = useUserNavigation();
 
   return (
     <div className="col bg-light mx-0 px-0">
@@ -55,7 +16,7 @@ const UserItem = (props: Props) => {
         <div className="row mx-0 px-0">
           <div className="col-auto p-3">
             <img
-              src={props.user.imageUrl}
+              src={user.imageUrl}
               className="img-fluid"
               width="80"
               alt="Posting user"
@@ -64,14 +25,14 @@ const UserItem = (props: Props) => {
           <div className="col">
             <h2>
               <b>
-                {props.user.firstName} {props.user.lastName}
+                {user.firstName} {user.lastName}
               </b>{" "}
               -{" "}
               <Link
-                to={`${props.featurePath}/${props.user.alias}`}
-                onClick={navigateToUser}
+                to={`${featurePath}/${user.alias}`}
+                onClick={(event) => navigateToUser(event, featurePath)}
               >
-                {props.user.alias}
+                {user.alias}
               </Link>
             </h2>
           </div>
